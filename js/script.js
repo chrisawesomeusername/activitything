7223954aeed687dcc756a1c3cf8bab92199ea1ae
chrisawesomeusername <chrisawesomeusername@gmail.com>
2020-02-10 20:39:18 +1300

    totals for two users

--- js/script.js
+++ js/script.js
@@ -18,4 +18,37 @@ document.getElementById('user1Total').addEventListener('click',function(){
   console.log(user1Total);
 
   document.getElementById('user1Result').value = user1Total;
+});
+
+document.getElementById('user2Total').addEventListener('click',function(){
+  var user2HourA = parseInt(document.getElementById('user2HourA').value);
+  var user2HourB = parseInt(document.getElementById('user2HourB').value);
+  var user2HourC = parseInt(document.getElementById('user2HourC').value);
+  var user2HourD = parseInt(document.getElementById('user2HourD').value);
+  var user2HourE = parseInt(document.getElementById('user2HourE').value);
+
+  console.log(user2HourA);
+  console.log(user2HourB);
+  console.log(user2HourC);
+  console.log(user2HourD);
+  console.log(user2HourE);
+
+  var user2Total = user2HourA + user2HourB + user2HourC + user2HourD + user2HourE;
+  console.log(user1Total);
+
+  document.getElementById('user2Result').value = user2Total;
 })
+
+
+// document.getElementById('user1Total').addEventListener('click',function(){
+//   var hours = [
+//     parseInt(document.getElementById('user1HourA').value),
+//     parseInt(document.getElementById('user1HourB').value),
+//     parseInt(document.getElementById('user1HourC').value),
+//     parseInt(document.getElementById('user1HourD').value),
+//     parseInt(document.getElementById('user1HourE').value)
+//   ];
+//   for (var i = 0; i < hours.length; i++) {
+//     console.log(hours);
+//   }
+// });
