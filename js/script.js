517e1f345f04f15bc78ac28eaf6715f7429cdea1
chrisawesomeusername <chrisawesomeusername@gmail.com>
2020-02-10 15:16:38 +1300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log('teest');
+
+
+document.getElementById('user1Total').addEventListener('click',function(){
+  var user1HourA = parseInt(document.getElementById('user1HourA').value);
+  var user1HourB = parseInt(document.getElementById('user1HourB').value);
+  var user1HourC = parseInt(document.getElementById('user1HourC').value);
+  var user1HourD = parseInt(document.getElementById('user1HourD').value);
+  var user1HourE = parseInt(document.getElementById('user1HourE').value);
+
+  console.log(user1HourA);
+  console.log(user1HourB);
+  console.log(user1HourC);
+  console.log(user1HourD);
+  console.log(user1HourE);
+
+  var user1Total = user1HourA + user1HourB + user1HourC + user1HourD + user1HourE;
+  console.log(user1Total);
+
+  document.getElementById('user1Result').innerHTML = '<p>' + user1Total +'</p>';
+})
